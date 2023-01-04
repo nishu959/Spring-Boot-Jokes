a415784281b1d321a421dada30a6ebf01cbcdf3b
nisha <nishantkumarsingh098@gmail.com>
2023-01-05 00:02:42 +0530

    added files

---
+++ src/test/java/guru/springframework/SpringBootJokes/SpringBootJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.SpringBootJokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
